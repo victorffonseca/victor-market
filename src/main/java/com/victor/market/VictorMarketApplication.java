87a0e2a0207c76cc555452c9bb8afe2162ac27d1
Victor Fonseca <victorf.fonseca01@gmail.com>
2021-12-06 15:58:09 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/victor/market/VictorMarketApplication.java
@@ -0,0 +1,13 @@
+package com.victor.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictorMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VictorMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/market/VictorMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VictorMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
