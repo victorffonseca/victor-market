16a40c6f3358fc7c3d570a80ed5f2e45f688ce4f
Victor Fonseca <victorf.fonseca01@gmail.com>
2021-12-07 16:49:01 -0500

    Exposición de endpoints para el API

--- src/main/java/com/victor/market/web/controller/ProductController.java
+++ src/main/java/com/victor/market/web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.victor.market.web.controller;
 import com.victor.market.domain.Product;
 import com.victor.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,23 +15,28 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping()
     public List<Product> getAll() {
         return productService.getAll();
     }
 
-    public Optional<Product> getProduct(int productId) {
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId) {
         return productService.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId) {
+    @GetMapping("/category/{id}")
+    public Optional<List<Product>> getByCategory(@PathVariable("id") int categoryId) {
         return productService.getByCategory(categoryId);
     }
 
-    public Product save(Product product) {
+    @PostMapping()
+    public Product save(@RequestBody Product product) {
         return productService.save(product);
     }
 
-    public boolean delete(int productId) {
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable("id") int productId) {
         return productService.delete(productId);
     }
 }
