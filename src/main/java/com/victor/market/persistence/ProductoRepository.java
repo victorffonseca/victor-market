4256086a8dfd1daee63bd34895f8f6bb895587e8
Victor Fonseca <victorf.fonseca01@gmail.com>
2021-12-07 15:18:12 -0500

    Implementación de repository para entidad producto

--- src/main/java/com/victor/market/persistence/ProductoRepository.java
+++ src/main/java/com/victor/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.victor.market.persistence;
 
 import com.victor.market.persistence.crud.ProductoCrudRepository;
 import com.victor.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -20,5 +22,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, Boolean.TRUE);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
