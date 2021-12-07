9fd4e503d293f8576997b7a8c6c463fcc0bb28b3
Victor Fonseca <victorf.fonseca01@gmail.com>
2021-12-07 14:33:24 -0500

    Se crean entidades para las tablas clientes, productos, compras y categorias

---
+++ src/main/java/com/victor/market/persistence/entity/Categoria.java
@@ -0,0 +1,41 @@
+package com.victor.market.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @Column(name = "id_categoria")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCategoria;
+
+    private String descripcion;
+
+    private Boolean estado;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/victor/market/persistence/entity/Cliente.java
@@ -0,0 +1,25 @@
+package com.victor.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    private String id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private Integer celular;
+
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+}
