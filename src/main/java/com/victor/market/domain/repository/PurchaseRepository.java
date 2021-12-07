d65d088bad8c968eb6f407559dbc925e012d859a
Victor Fonseca <victorf.fonseca01@gmail.com>
2021-12-07 17:05:55 -0500

    Creación de modelo de dominio para compras

---
+++ src/main/java/com/victor/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,15 @@
+package com.victor.market.domain.repository;
+
+import com.victor.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+
+    List<Purchase> getAll();
+
+    Optional<List<Purchase>> getByClient(String clientId);
+
+    Purchase save(Purchase purchase);
+}
--- src/main/java/com/victor/market/persistence/entity/Compra.java
+++ src/main/java/com/victor/market/persistence/entity/Compra.java
@@ -23,7 +23,7 @@ public class Compra {
 
     private String comentario;
 
-    private Boolean estado;
+    private String estado;
 
     @ManyToOne
     @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
@@ -72,11 +72,11 @@ public class Compra {
         this.comentario = comentario;
     }
 
-    public Boolean getEstado() {
+    public String getEstado() {
         return estado;
     }
 
-    public void setEstado(Boolean estado) {
+    public void setEstado(String estado) {
         this.estado = estado;
     }
 }
