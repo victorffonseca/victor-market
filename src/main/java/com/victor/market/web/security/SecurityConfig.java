8a14aad7043e5c47feee96b3f2d32e6e34bec7b0
Victor Fonseca <victorf.fonseca01@gmail.com>
2021-12-08 16:01:15 -0500

    Adición de seguridad por usuario y contraseña al API

---
+++ src/main/java/com/victor/market/web/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.victor.market.web.security;
+
+import com.victor.market.domain.service.VictorUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private VictorUserDetailsService victorUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(victorUserDetailsService);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs/**");
+        web.ignoring().antMatchers("/swagger.json");
+        web.ignoring().antMatchers("/swagger-ui.html");
+        web.ignoring().antMatchers("/swagger-resources/**");
+        web.ignoring().antMatchers("/webjars/**");
+    }
+}
