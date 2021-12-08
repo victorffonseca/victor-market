49dfa825010232ec38ef3912589ed92aa1116eab
Victor Fonseca <victorf.fonseca01@gmail.com>
2021-12-08 10:04:31 -0500

    Ajuste bug tipo de dato para celular cliente

--- src/main/java/com/victor/market/persistence/entity/Cliente.java
+++ src/main/java/com/victor/market/persistence/entity/Cliente.java
@@ -14,7 +14,7 @@ public class Cliente {
 
     private String apellidos;
 
-    private Integer celular;
+    private Long celular;
 
     private String direccion;
 
@@ -48,11 +48,11 @@ public class Cliente {
         this.apellidos = apellidos;
     }
 
-    public Integer getCelular() {
+    public Long getCelular() {
         return celular;
     }
 
-    public void setCelular(Integer celular) {
+    public void setCelular(Long celular) {
         this.celular = celular;
     }
 
